17ab53b05ec5813b6545f63e6dc9472e74f78ecf
Chirag Patel <chiragpatel639@gmail.com>
2015-02-01 17:49:09 -0500

    WelCome

---
+++ CricketApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CricketApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to My Cricket App";
+
+            return View();
+        }
+
+        public ActionResult Match()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult Series()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact Me";
+
+            return View();
+        }
+    }
+}
